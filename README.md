4c4d603e01d4fbbe6b0647473c904cf27e822ab7
Ralica1 <rasim.korajlic@gmail.com>
2023-01-03 16:08:41 +0100

    Created variables and starter logic

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ Set up the HTML structure for the Tic Tac Toe game.
 
 ### 2nd step
 
-Styled the game with a dark theme and made it responsive.
+Styled the game with a dark theme.
 
 ### 3rd step 
 
 The board element has an event listener added to it that listens for clicks and calls the handleClick function when a cell is clicked. The handleClick function places the current player's mark on the board and checks if the player has won the game by calling the checkForWin function. If the player has won, the gameOver variable is set to true to end the game. If the player has not won, the function switches to the other player by toggling the value of the currentPlayer variable.
+
+### 4th step 
 
--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 const board = document.querySelector('.board');
-let cells = document.querySelectorAll('.cell');
-let currentPlayer = 1; // Player 1 goes first
-let gameOver = false;
+const cells = document.querySelectorAll('.cell');
+const statusDisplay = document.querySelector('.game--status');
+const newGameButton = document.querySelector('.game--restart');
+const player1Scoreboard = document.querySelector('.player-1-score');
+const player2Scoreboard = document.querySelector('.player-2-score');
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 let player1Score = 0;
 let player2Score = 0;
 
-board.addEventListener('click', handleClick);
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-function handleClick(event) {
-  // Check if the game is over or if the cell has already been clicked
-  if (gameOver || event.target.textContent) {
-    return;
-  }
+statusDisplay.innerHTML = currentPlayerTurn();
 
-  // Place the player's mark on the board
-  event.target.textContent = currentPlayer === 1 ? 'X' : 'O';
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-  // Check if the player has won
-  if (checkForWin()) {
-    gameOver = true;
-    return;
-  }
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 
-  // Switch to the other player
-  currentPlayer = currentPlayer === 1 ? 2 : 1;
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
