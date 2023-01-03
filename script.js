cc3b14b85ed3e3a36202867647a4462d323dc615
Ralica1 <rasim.korajlic@gmail.com>
2023-01-04 00:28:13 +0100

    Added function to check for draw and another one to keep score

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const gameboard = ["", "", "", "", "", "", "", "", ""];
+let player1Score = 0;
+let player2Score = 0;
+let currentPlayer = "X";
+
 function renderGameboard(gameboard) {
-  const cells = document.querySelectorAll('.cell');
+  const cells = document.querySelectorAll(".cell");
   cells.forEach((cell, index) => {
     cell.textContent = gameboard[index];
   });
 }
-const gameboard = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
 
 function placeMark(gameboard, index, mark) {
   if (gameboard[index] === "") {
@@ -24,35 +27,50 @@ function handleCellClick(event) {
   }
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => cell.addEventListener("click", handleCellClick));
 
 function checkForWin(gameboard) {
   // Check rows
   for (let i = 0; i < 3; i++) {
-    if (gameboard[i * 3] !== "" && gameboard[i * 3] === gameboard[i * 3 + 1] && gameboard[i * 3 + 1] === gameboard[i * 3 + 2]) {
+    if (
+      gameboard[i * 3] !== "" &&
+      gameboard[i * 3] === gameboard[i * 3 + 1] &&
+      gameboard[i * 3 + 1] === gameboard[i * 3 + 2]
+    ) {
       return gameboard[i * 3];
     }
   }
 
   // Check columns
   for (let i = 0; i < 3; i++) {
-    if (gameboard[i] !== "" && gameboard[i] === gameboard[i + 3] && gameboard[i + 3] === gameboard[i + 6]) {
+    if (
+      gameboard[i] !== "" &&
+      gameboard[i] === gameboard[i + 3] &&
+      gameboard[i + 3] === gameboard[i + 6]
+    ) {
       return gameboard[i];
     }
   }
 
   // Check diagonals
-  if (gameboard[0] !== "" && gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8]) {
+  if (
+    gameboard[0] !== "" &&
+    gameboard[0] === gameboard[4] &&
+    gameboard[4] === gameboard[8]
+  ) {
     return gameboard[0];
   }
-  if (gameboard[2] !== "" && gameboard[2] === gameboard[4] && gameboard[4] === gameboard[6]) {
+  if (
+    gameboard[2] !== "" &&
+    gameboard[2] === gameboard[4] &&
+    gameboard[4] === gameboard[6]
+  ) {
     return gameboard[2];
   }
 
   // Check for tie
-  if (gameboard.every(cell => cell !== "")) {
+  if (gameboard.every((cell) => cell !== "")) {
     return "T";
   }
 
@@ -68,8 +86,19 @@ function handleCellClick(event) {
     const result = checkForWin(gameboard);
     if (result !== null) {
       const resultElement = document.createElement('div');
-      resultElement.textContent = `Game over! ${result} wins!`;
+      if (result === "T") {
+        resultElement.textContent = "Game over! It's a tie!";
+      } else {
+        resultElement.textContent = `Game over! ${result} wins!`;
+        if (result === "X") {
+          player1Score++;
+        } else if (result === "O") {
+          player2Score++;
+        }
+      }
       document.querySelector('#game-result').appendChild(resultElement);
+      document.querySelector('#game-score-1').textContent = player1Score;
+      document.querySelector('#game-score-2').textContent = player2Score;
     }
   }
 }
@@ -77,9 +106,9 @@ function handleCellClick(event) {
 function resetGame() {
   gameboard.fill("");
   currentPlayer = "X";
-  cells.forEach(cell => cell.textContent = "");
-  document.querySelector('#game-result').textContent = "";
+  cells.forEach((cell) => (cell.textContent = ""));
+  document.querySelector("#game-result").textContent = "";
 }
 
-const newGameButton = document.querySelector('#new-game');
-newGameButton.addEventListener('click', resetGame);
+const newGameButton = document.querySelector("#new-game");
+newGameButton.addEventListener("click", resetGame);
