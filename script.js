f0daed7ff3a7aae8ffecef1e397621e3382bd7e7
Ralica1 <rasim.korajlic@gmail.com>
2023-01-03 18:21:33 +0100

    Added ability to add marks to the board

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function renderGameboard(gameboard) {
     cell.textContent = gameboard[index];
   });
 }
+const gameboard = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+
+function placeMark(gameboard, index, mark) {
+  if (gameboard[index] === "") {
+    gameboard[index] = mark;
+    return true;
+  }
+  return false;
+}
+
+function handleCellClick(event) {
+  const index = event.target.dataset.index;
+  const success = placeMark(gameboard, index, currentPlayer);
+  if (success) {
+    event.target.textContent = currentPlayer;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
