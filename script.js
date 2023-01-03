ff3d32a70e871e3b24171b8f87e5355b58ccd008
Ralica1 <rasim.korajlic@gmail.com>
2023-01-03 17:40:54 +0100

    Added function to display gameboard on page

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-const board = document.querySelector('.board');
-const cells = document.querySelectorAll('.cell');
-const statusDisplay = document.querySelector('.game--status');
-const newGameButton = document.querySelector('.game--restart');
-const player1Scoreboard = document.querySelector('.player-1-score');
-const player2Scoreboard = document.querySelector('.player-2-score');
-
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
-let player1Score = 0;
-let player2Score = 0;
-
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-statusDisplay.innerHTML = currentPlayerTurn();
-
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
-}
-
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
+function renderGameboard(gameboard) {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell, index) => {
+    cell.textContent = gameboard[index];
+  });
 }
