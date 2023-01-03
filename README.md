9b45cac7a9cd32360e55bd92681f6c21928d217d
Ralica1 <rasim.korajlic@gmail.com>
2023-01-03 02:29:54 +0100

    Implement game logic and event handlers

--- README.md
+++ README.md
@@ -8,24 +8,8 @@ Set up the HTML structure for the Tic Tac Toe game.
 
 Styled the game with a dark theme and made it responsive.
 
-### 3rd step
+### 3rd step 
 
-In this step, I set up the HTML for the gameboard and wrote a JavaScript function that renders the contents of the gameboard array to the webpage. I also filled in the array with "X"s and "O"s for testing purposes.
-
-To render the gameboard, I first created an HTML table and added a row and three cells for each element in the gameboard array. I then added a `data-index` attribute to each cell, which will be used later to identify which cell was clicked by the player.
-
-Next, I wrote a JavaScript function called `renderGameboard()` that loops through the gameboard array and updates the text content of each cell to match the corresponding element in the array. I also added this function to the `window.onload` event to render the gameboard as soon as the page loads.
-
-Now, the gameboard is displayed on the webpage and the cells are updated with the values in the gameboard array.
-
-### 4th step
-
-In this step, I built the functions that allow players to add their marks to a specific spot on the gameboard and tied them to the DOM. I also added the logic that prevents players from playing in spots that are already taken.
-
-To allow players to add their marks to the gameboard, I first added a click event listener to the cells of the gameboard. When a cell is clicked, I retrieve the `data-index` attribute of the cell and use it to determine the corresponding element in the gameboard array.
-
-Next, I wrote a function called `addMark()` that takes the index of the cell as an argument and adds the player's mark to the gameboard array. I also added a check to ensure that the cell is not already taken before allowing the player to add their mark.
-
-Finally, I updated the `renderGameboard()` function to re-render the gameboard after the mark is added, so that the changes are reflected on the webpage.
-
-Now, players can click on the gameboard to place their marks and the gameboard is updated to reflect their moves.
+- Implemented the game logic to track the state of the gameboard, switch players, and check for a winner or a draw
+- Added event listeners to handle cell clicks and update the gameboard state and UI accordingly
+- Added possibility to add name
--- script.js
+++ script.js
@@ -1,31 +1,134 @@
-function renderGameboard() {
-  // Get the gameboard cells
-  const cells = document.querySelectorAll("#gameboard td");
+// Initialize constants for the gameboard and cells
+const gameboard = document.querySelector("#gameboard");
+const cells = gameboard.querySelectorAll("td");
 
-  // Loop through the gameboard array and update the text content of each cell
-  for (let i = 0; i < gameboard.length; i++) {
-    cells[i].textContent = gameboard[i];
+// Initialize variables for the players and current player
+let player1 = "";
+let player2 = "";
+let currentPlayer = "X";
+
+// Initialize a 2D array to track the state of the gameboard
+let gameboardState = [  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+// Initialize a variable to track the game's status (whether it's ongoing or ended)
+let gameEnded = false;
+
+// Create a function to switch the current player
+const switchPlayer = () => {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
   }
-}
-// Bind the addMark() function to the click event of the gameboard cells
-document.querySelectorAll("#gameboard td").forEach(cell => {
-  cell.addEventListener("click", function(event) {
-    // Get the index of the cell that was clicked
-    const index = event.target.getAttribute("data-index");
-    // Add the player's mark to the gameboard
-    addMark(index);
-  });
+};
+
+// Create a function to update the gameboard state and the UI
+const updateGameboard = (row, col) => {
+  gameboardState[row][col] = currentPlayer;
+  cells[3 * row + col].textContent = currentPlayer;
+};
+
+// Create a function to check if the game has ended
+const checkGameEnd = () => {
+  // Check rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameboardState[i][0] !== null &&
+      gameboardState[i][0] === gameboardState[i][1] &&
+      gameboardState[i][1] === gameboardState[i][2]
+    ) {
+      return gameboardState[i][0];
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameboardState[0][i] !== null &&
+      gameboardState[0][i] === gameboardState[1][i] &&
+      gameboardState[1][i] === gameboardState[2][i]
+    ) {
+      return gameboardState[0][i];
+    }
+  }
+
+  // Check diagonals
+  if (
+    gameboardState[0][0] !== null &&
+    gameboardState[0][0] === gameboardState[1][1] &&
+    gameboardState[1][1] === gameboardState[2][2]
+  ) {
+    return gameboardState[0][0];
+  }
+  if (
+    gameboardState[0][2] !== null &&
+    gameboardState[0][2] === gameboardState[1][1] &&
+    gameboardState[1][1] === gameboardState[2][0]
+  ) {
+    return gameboardState[0][2];
+  }
+
+  // Check for a draw
+  if (gameboardState.flat().every(cell => cell !== null)) {
+    return "D";
+  }
+
+  // Game is ongoing
+  return null;
+};
+// Create a function to handle cell clicks
+const handleCellClick = event => {
+  // Get the row and column of the clicked cell
+  const row = event.target.parentNode.rowIndex;
+  const col = event.target.cellIndex;
+
+  // Ignore the click if the game has ended or the cell has already been clicked
+  if (gameEnded || gameboardState[row][col] !== null) {
+    return;
+  }
+
+  // Update the gameboard state and the UI
+  updateGameboard(row, col);
+
+  // Check if the game has ended
+  const winner = checkGameEnd();
+  if (winner === "X" || winner === "O") {
+    // Display the winner
+    document.querySelector("#result").textContent = `${winner} wins!`;
+    gameEnded = true;
+  } else if (winner === "D") {
+    // Display the draw
+    document.querySelector("#result").textContent = "It's a draw!";
+    gameEnded = true;
+  } else {
+    // Switch the current player
+    switchPlayer();
+  }
+};
+
+// Add event listeners for cell clicks
+cells.forEach(cell => cell.addEventListener("click", handleCellClick));
+
+// Get the start/restart button and add an event listener for clicks
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+  // Get the player names
+  player1 = document.querySelector("#player1").value;
+  player2 = document.querySelector("#player2").value;
+
+  // Reset the game
+  currentPlayer = "X";
+  gameboardState = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+  gameEnded = false;
+
+  // Clear the cells and result display
+  cells.forEach(cell => (cell.textContent = ""));
+  document.querySelector("#result").textContent = "";
 });
-
-// Add the player's mark to the gameboard at the specified index
-function addMark(index) {
-  // Check if the cell is already taken
-  if (gameboard[index] === "") {
-    // Add the player's mark to the gameboard array
-    gameboard[index] = currentPlayer;
-    // Switch to the other player
-    currentPlayer = (currentPlayer === "X") ? "O" : "X";
-    // Render the updated gameboard
-    renderGameboard();
-  }
-}
